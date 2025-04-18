464613684b1df5a65ffe33c6e0a97e795b64d44a
Cole-Godfrey <colegodfrey34@gmail.com>
2025-04-18 16:01:36 -0700

    Defined board class and tried to render board on screen, have not gotten that to work yet though.

---
+++ Board.hpp
@@ -0,0 +1,11 @@
+#include "Piece.hpp"
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+class Board {
+public:
+	
+private:
+	Piece board[8][8];
+};
--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Image.hpp>
+
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(1000, 1000), "CHESS 2");
+    Texture chessBoard;
+    chessBoard.loadFromFile("board.jpg");
+
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        Sprite board;
+        board.setTexture(chessBoard);
+        window.draw(board);
         window.display();
     }
 
