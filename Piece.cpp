e46706848adc5787482ce7b5e468f6614f8c6b78
Cal Tatualla <matatualla@gmail.com>
2025-04-20 19:06:28 -0700

    istfg just push it

--- Piece.cpp
+++ Piece.cpp
@@ -11,20 +11,32 @@ std::vector<Pair> Piece::getAllPossibleMoves(Pair, int[8][8]) {
 
 std::vector<Pair> Pawn::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     std::vector<Pair> moves;
-    int dir = (board[curPos.y][curPos.x] > 0) ? -1 : 1;
+    int direction;
 
-    if (isInBounds(curPos.x, curPos.y + dir) && board[curPos.y + dir][curPos.x] == 0)
-        moves.push_back({ curPos.x, curPos.y + dir });
+    if (board[curPos.y][curPos.x] > 0)
+        direction = -1; 
+    else
+        direction = 1;  
 
-    if (((dir == -1 && curPos.y == 6) || (dir == 1 && curPos.y == 1)) && board[curPos.y + dir][curPos.x] == 0 &&
-        board[curPos.y + 2 * dir][curPos.x] == 0)
-        moves.push_back({ curPos.x, curPos.y + 2 * dir });
+    if (isInBounds(curPos.x, curPos.y + direction) && board[curPos.y + direction][curPos.x] == 0)
+        moves.push_back({ curPos.x, curPos.y + direction });
 
+    // first move of pawn that they can hop in twice
+    if (((direction == -1 && curPos.y == 6) || (direction == 1 && curPos.y == 1)) &&
+        board[curPos.y + direction][curPos.x] == 0 &&
+        board[curPos.y + 2 * direction][curPos.x] == 0)
+        moves.push_back({ curPos.x, curPos.y + 2 * direction });
+
+    // Eating diagonally only
     for (int dx = -1; dx <= 1; dx += 2) {
-        int nx = curPos.x + dx, ny = curPos.y + dir;
-        if (isInBounds(nx, ny) && board[ny][nx] != 0 &&
-            (board[ny][nx] > 0) != (board[curPos.y][curPos.x] > 0))
-            moves.push_back({ nx, ny });
+        int nx = curPos.x + dx;
+        int ny = curPos.y + direction;
+        if (isInBounds(nx, ny) && board[ny][nx] != 0) {
+            bool isCurrentWhite = board[curPos.y][curPos.x] > 0;
+            bool isTargetWhite = board[ny][nx] > 0;
+            if (isCurrentWhite != isTargetWhite)
+                moves.push_back({ nx, ny });
+        }
     }
 
     return moves;
@@ -34,12 +46,26 @@ std::vector<Pair> Knight::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     std::vector<Pair> moves;
     int dx[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
     int dy[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
-    int side = (board[curPos.y][curPos.x] > 0) ? 1 : -1;
+    int side;
+
+    if (board[curPos.y][curPos.x] > 0)
+        side = 1;
+    else
+        side = -1;
 
     for (int i = 0; i < 8; ++i) {
-        int nx = curPos.x + dx[i], ny = curPos.y + dy[i];
-        if (isInBounds(nx, ny) && (board[ny][nx] == 0 || (board[ny][nx] > 0) != (side > 0)))
-            moves.push_back({ nx, ny });
+        int nx = curPos.x + dx[i];
+        int ny = curPos.y + dy[i];
+        if (isInBounds(nx, ny)) {
+            if (board[ny][nx] == 0)
+                moves.push_back({ nx, ny });
+            else {
+                bool isCurrentWhite = side > 0;
+                bool isTargetWhite = board[ny][nx] > 0;
+                if (isCurrentWhite != isTargetWhite)
+                    moves.push_back({ nx, ny });
+            }
+        }
     }
 
     return moves;
@@ -47,36 +73,63 @@ std::vector<Pair> Knight::getAllPossibleMoves(Pair curPos, int board[8][8]) {
 
 std::vector<Pair> Bishop::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     std::vector<Pair> moves;
-    int side = (board[curPos.y][curPos.x] > 0) ? 1 : -1;
+    int side;
 
-    for (int dx = -1; dx <= 1; dx += 2)
-        for (int dy = -1; dy <= 1; dy += 2)
+    if (board[curPos.y][curPos.x] > 0)
+        side = 1;
+    else
+        side = -1;
+
+    for (int dx = -1; dx <= 1; dx += 2) {
+        for (int dy = -1; dy <= 1; dy += 2) {
             for (int i = 1; i < 8; ++i) {
-                int nx = curPos.x + i * dx, ny = curPos.y + i * dy;
-                if (!isInBounds(nx, ny)) break;
+
+                int nx = curPos.x + i * dx;
+                int ny = curPos.y + i * dy;
+
+                if (!isInBounds(nx, ny))
+                    break;
                 int val = board[ny][nx];
-                if (val == 0) moves.push_back({ nx, ny });
+
+                if (val == 0)
+                    moves.push_back({ nx, ny });
+
                 else {
-                    if ((val > 0) != (side > 0)) moves.push_back({ nx, ny });
+                    bool isCurrentWhite = side > 0;
+                    bool isTargetWhite = val > 0;
+                    if (isCurrentWhite != isTargetWhite)
+                        moves.push_back({ nx, ny });
                     break;
                 }
             }
+        }
+    }
 
     return moves;
 }
 
 std::vector<Pair> Rook::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     std::vector<Pair> moves;
-    int side = (board[curPos.y][curPos.x] > 0) ? 1 : -1;
+    int side;
+
+    if (board[curPos.y][curPos.x] > 0)
+        side = 1;
+    else
+        side = -1;
 
     for (int dx = -1; dx <= 1; dx += 2) {
         for (int i = 1; i < 8; ++i) {
             int nx = curPos.x + i * dx;
-            if (!isInBounds(nx, curPos.y)) break;
+            if (!isInBounds(nx, curPos.y))
+                break;
             int val = board[curPos.y][nx];
-            if (val == 0) moves.push_back({ nx, curPos.y });
+            if (val == 0)
+                moves.push_back({ nx, curPos.y });
             else {
-                if ((val > 0) != (side > 0)) moves.push_back({ nx, curPos.y });
+                bool isCurrentWhite = side > 0;
+                bool isTargetWhite = val > 0;
+                if (isCurrentWhite != isTargetWhite)
+                    moves.push_back({ nx, curPos.y });
                 break;
             }
         }
@@ -85,11 +138,16 @@ std::vector<Pair> Rook::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     for (int dy = -1; dy <= 1; dy += 2) {
         for (int i = 1; i < 8; ++i) {
             int ny = curPos.y + i * dy;
-            if (!isInBounds(curPos.x, ny)) break;
+            if (!isInBounds(curPos.x, ny))
+                break;
             int val = board[ny][curPos.x];
-            if (val == 0) moves.push_back({ curPos.x, ny });
+            if (val == 0)
+                moves.push_back({ curPos.x, ny });
             else {
-                if ((val > 0) != (side > 0)) moves.push_back({ curPos.x, ny });
+                bool isCurrentWhite = side > 0;
+                bool isTargetWhite = val > 0;
+                if (isCurrentWhite != isTargetWhite)
+                    moves.push_back({ curPos.x, ny });
                 break;
             }
         }
@@ -99,27 +157,43 @@ std::vector<Pair> Rook::getAllPossibleMoves(Pair curPos, int board[8][8]) {
 }
 
 std::vector<Pair> Queen::getAllPossibleMoves(Pair curPos, int board[8][8]) {
-	// same shit as the bishop and rook combined
+    //  same sht as rook and bish but combined
     Bishop b;
     Rook r;
-    auto moves = b.getAllPossibleMoves(curPos, board);
-    auto r_moves = r.getAllPossibleMoves(curPos, board);
-    moves.insert(moves.end(), r_moves.begin(), r_moves.end());
+    std::vector<Pair> moves = b.getAllPossibleMoves(curPos, board);
+    std::vector<Pair> rookMoves = r.getAllPossibleMoves(curPos, board);
+    moves.insert(moves.end(), rookMoves.begin(), rookMoves.end());
     return moves;
 }
 
 std::vector<Pair> King::getAllPossibleMoves(Pair curPos, int board[8][8]) {
     std::vector<Pair> moves;
-    int side = (board[curPos.y][curPos.x] > 0) ? 1 : -1;
+    int side;
 
-    for (int dx = -1; dx <= 1; ++dx)
+    if (board[curPos.y][curPos.x] > 0)
+        side = 1;
+    else
+        side = -1;
+
+    for (int dx = -1; dx <= 1; ++dx) {
         for (int dy = -1; dy <= 1; ++dy) {
-            if (dx == 0 && dy == 0) continue;
-            int nx = curPos.x + dx, ny = curPos.y + dy;
-            if (isInBounds(nx, ny) &&
-                (board[ny][nx] == 0 || (board[ny][nx] > 0) != (side > 0)))
-                moves.push_back({ nx, ny });
+            if (dx == 0 && dy == 0)
+                continue;
+            int nx = curPos.x + dx;
+            int ny = curPos.y + dy;
+            if (isInBounds(nx, ny)) {
+                int val = board[ny][nx];
+                if (val == 0)
+                    moves.push_back({ nx, ny });
+                else {
+                    bool isCurrentWhite = side > 0;
+                    bool isTargetWhite = val > 0;
+                    if (isCurrentWhite != isTargetWhite)
+                        moves.push_back({ nx, ny });
+                }
+            }
         }
+    }
 
     return moves;
 }
